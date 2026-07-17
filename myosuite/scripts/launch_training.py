df78b57ef91f1af6b652781d54f78364feec8109
Sophia Taddei <sophia.taddei@vub.be>
2026-07-17 15:39:52 +0200

    resolved env: conda then uv sync and uv pip install -e .

--- myosuite/scripts/launch_training.py
+++ myosuite/scripts/launch_training.py
@@ -1,11 +1,22 @@
+import os
+import sys
+
+# Ajoute le dossier racine du projet et le sous-dossier myosuite au PATH de Python
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.abspath(os.path.join(current_dir, ".."))
+
+sys.path.insert(0, root_dir)
+sys.path.insert(0, os.path.join(root_dir, "myosuite"))
+
+
 from myosuite.my_implementation.training.trainer import train_model_imitation
 
 if __name__ == "__main__":
     model = train_model_imitation(
-        run_name="test",
+        run_name="testALLLEZ",
         weighted_reward_keys={"sparse": 0, "solved": 10, "qpos_imitation": 30, "forward_bounded": 10, "alive_bonus": 15,
         "fall_penalty": -50,},
-        total_timesteps=1_000_000,
+        total_timesteps=5_000_000,
         net_arch=dict(pi=[256, 256], vf=[256, 256]),
         seed=42,
         n_envs=8,
